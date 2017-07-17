f5eca4323e109556fe32aff42a6351ffba6570de
Izaak <izaak.lakatorz@tieto.com>
2017-07-17 11:47:03 +0200

    choose lang

---
+++ main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <string.h>
+
+int ChooseLang (char* opt) {
+	if (strcmp(opt,"en")==0) {
+	printf("Hello Mr. Smith.\n");
+	}
+	else if (strcmp(opt,"pl")==0) {
+	printf("Witaj Panie Jerzyna.\n");
+	}
+	return 0;
+}
+
+int main (int argc, char* argv[]) {
+	ChooseLang(argv[1]);
+
+	
+
+}
